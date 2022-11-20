f363c91b9207aa60adcf66d9c3a52107fe2954a8
Lesli Ramos <les11@email.unc.edu>
2022-11-20 13:19:40 -0500

    changed server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import roll from './lib/roll.js';
+import {roll} from './lib/roll.js';
 
 import express from 'express';
 
@@ -7,7 +7,7 @@ import minimist from 'minimist';
 const app = express();
 const args = minimist(process.argv.slice(2));
 
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 var port = 5000;
@@ -15,38 +15,45 @@ if(args.port) {
 	port = args.port;
 }
 
-var sides = 6;
-var dice = 2;
-var rolls = 1;
+//var sides = 6;
+//var dice = 2;
+//var rolls = 1;
 
 app.get('/app/', (req, res) => {
 	res.send("200 OK");
 })
 
-app.use('/app/roll/', (req, res, next) => {
-	res.send(roll(sides, dice, rolls)).end();
+app.get('/app/roll/', (req, res) => {
+	res.send(roll(6,2,1));
 })
 
-app.get('/app/roll/:sides', (req, res, next) => {
-	sides = parseInt(req.params.sides);
-	res.send(roll(sides, dice, rolls)).end();
+app.post('/app/roll/', (req, res) => {
+	let sides = parseInt(req.body.sides);
+	let dice = parseInt(req.body.dice);
+	let rolls = parseInt(req.body.rolls);
+	res.send(roll(sides, dice, rolls));
+});
+
+app.get('/app/roll/:sides/', (req, res) => {
+	let sides = parseInt(req.params.sides);
+	res.send(roll(sides,2,1));
 })
 
-app.get('/app/roll/:sides/:dice/', (req, res, next) => {
-	sides = parseInt(req.params.sides);
-	dice = parseInt(req.params.dice);
-	res.send(roll(sides, dice, rolls)).end();
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+	let sides = parseInt(req.params.sides);
+	let dice = parseInt(req.params.dice);
+	res.send(roll(sides, dice,1));
 })
 
-app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
-	sides = parseInt(req.params.sides);
-	dice = parseInt(req.params.dice);
-	rolls = parseInt(req.params.rolls);
-	res.send(roll(sides, dice, rolls)).end();
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+	let sides = parseInt(req.params.sides);
+	let dice = parseInt(req.params.dice);
+	let rolls = parseInt(req.params.rolls);
+	res.send(roll(sides, dice, rolls));
 })
 
 app.use((req, res) => {
-	res.status(404).send("404 NOT FOUND").end();
+	res.status(404).send("404 NOT FOUND");
 })
 
 app.listen(port);
